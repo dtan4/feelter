f507e42e7cba648581c5f3d5e0f11099a7b02fcd
dtan4 <dtanshi45@gmail.com>
2014-06-09 02:19:47 +0900

    Parse both RSS and Atom feed

--- spec/feelter/feed_spec.rb
+++ spec/feelter/feed_spec.rb
@@ -5,18 +5,47 @@ WebMock.allow_net_connect!
 
 module Feelter
   describe Feed do
-    let(:feed) { described_class.new }
-    let(:feed_url) { "http://example.com/sample.xml" }
-    let(:xml) { open(fixture_path("sample.xml")).read }
+    let(:feed) do
+      described_class.new
+    end
 
     describe "#get_feed" do
-      before do
-        stub_request(:get, feed_url).to_return(body: xml, status: 200)
+      context "with RSS 2.0" do
+        let(:feed_url) do
+          "http://example.com/rss.xml"
+        end
+
+        let(:xml) do
+          open(fixture_path("rss.xml")).read
+        end
+
+        before do
+          stub_request(:get, feed_url).to_return(body: xml, status: 200)
+        end
+
+        it "should get feed xml" do
+          feed.get_feed(feed_url)
+          expect(feed.instance_variable_get(:@source).title).to eq "Sample RSS"
+        end
       end
 
-      it "should get feed xml" do
-        feed.get_feed(feed_url)
-        expect(feed.instance_variable_get(:@source).title).to eq "RubyFlow"
+      context "with Atom" do
+        let(:feed_url) do
+          "http://example.com/atom.xml"
+        end
+
+        let(:xml) do
+          open(fixture_path("atom.xml")).read
+        end
+
+        before do
+          stub_request(:get, feed_url).to_return(body: xml, status: 200)
+        end
+
+        it "should get feed xml" do
+          feed.get_feed(feed_url)
+          expect(feed.instance_variable_get(:@source).title).to eq "Sample Atom"
+        end
       end
     end
   end
