713f35eb49e53a0af2e561a78c158102ab288e46
dtan4 <dtanshi45@gmail.com>
2014-06-24 16:10:11 +0900

    Implement filter of RSS

--- lib/feelter/parser/rss.rb
+++ lib/feelter/parser/rss.rb
@@ -8,6 +8,25 @@ module Feelter
       def initialize(xml)
         @doc = Nokogiri::XML.parse(xml)
       end
+
+      def exclude_by(query)
+        raise ArgumentError if query.nil?
+        query.each { |key, value| filter_xml(key, value) }
+        @doc.to_s
+      end
+
+      private
+
+      def filter_xml(key, value)
+        items.each do |item|
+          next unless item.css(key.to_s)
+          item.remove if /#{value}/ =~ item.css(key.to_s).text
+        end
+      end
+
+      def items
+        @doc.css("item")
+      end
     end
   end
 end
--- spec/feelter/parser/rss_spec.rb
+++ spec/feelter/parser/rss_spec.rb
@@ -11,6 +11,10 @@ module Feelter
         open(fixture_path("rss.xml")).read
       end
 
+      let(:filtered_rss) do
+        open(fixture_path("rss_filtered.xml")).read
+      end
+
       describe "#parsable?" do
         let(:parsable) do
           described_class.parsable?(xml)
@@ -40,6 +44,34 @@ module Feelter
       describe "#initialize" do
         it "should have a instance of Nokogiri::XML::Document" do
           expect(instance.instance_variable_get(:@doc)).to be_a Nokogiri::XML::Document
+        end
+      end
+
+      describe "#exclude_by" do
+        let(:exclude_by) do
+          instance.exclude_by(arg)
+        end
+
+        context "nil is given" do
+          let(:arg) do
+            nil
+          end
+
+          it "should raise ArgumentError" do
+            expect do
+              exclude_by
+            end.to raise_error ArgumentError
+          end
+        end
+
+        context "search query is given" do
+          let(:arg) do
+            { title: 3 }
+          end
+
+          it "should return filtered XML" do
+            expect(exclude_by.gsub(/\s/, "")).to eq filtered_rss.gsub(/\s/, "")
+          end
         end
       end
     end
